a06df800214a582796a6a27db18114ad485348f2
DomePwny <Bababoss17@hotmail.de>
2016-08-23 15:29:49 +0200

    Ui.hpp

--- 2ndLife.KelleysIsland/dialog/ui.hpp
+++ 2ndLife.KelleysIsland/dialog/ui.hpp
@@ -1022,19 +1022,19 @@
 			class downtime
 			{
 				type=CT_STRUCTURED_TEXT;
+				style=0x0c+0x02;
 				idc=44444;
-				style=ST_LEFT;
 				x = 0.01 * safezoneW + safezoneX;
 				y = 0.200 * safezoneH + safezoneY;
-				w = 0.5;
-				h = 0.5;
-				sizeEx=0.03;
-				size=1;
-				font="PuristaLight";
-				colorBackground[]={0,0,0,0};
+				w = 0.55;
+				h = 0.11;
+				sizeEx=0.035;
+				size=0.035;
+				font="PuristaMedium";
+				colorBackground[]={0,0,0,0.6};
 				colorText[] = { 1 , 1 , 1 , 1 };
 				shadow=false;
 				text="";
-			};	
+			};
 		};   			 
  	};
